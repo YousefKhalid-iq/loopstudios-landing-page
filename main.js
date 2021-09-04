8bb32a803c07129afd443d5269acb22ca3a079dd
Yousef Khalid <84613950+YousefKhalid-iq@users.noreply.github.com>
2021-09-04 18:13:18 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
-const share = document.querySelector(".share-img-bg");
-const active = document.querySelector(".share-change");
+const toggleOn = document.querySelector(".toggle-on-cont");
+const active = document.querySelector(".menu-container");
 
-share.addEventListener("click", function () {
+const toggleOff = document.querySelector(".toggle-off-cont");
+
+const logo = document.querySelector(".logo-img");
+
+
+toggleOn.addEventListener("click", function () {
 	active.classList.toggle("press");
 });
+
+toggleOn.addEventListener("click", function () {
+	toggleOff.classList.toggle("press2");
+});
+
+toggleOn.addEventListener("click", function() {
+	toggleOn.classList.toggle("press3")
+});
+
+toggleOff.addEventListener("click", function() {
+	active.classList.toggle("press")
+});
+
+toggleOff.addEventListener("click", function() {
+	toggleOff.classList.toggle("press2")
+});
+
+toggleOff.addEventListener("click", function() {
+	toggleOn.classList.toggle("press3")
+});
+
+toggleOn.addEventListener("click", function () {
+	logo.classList.toggle("press4")
+});
+
+toggleOff.addEventListener("click", function () {
+	logo.classList.toggle("press4")
+});
+	
+
+
+
+
